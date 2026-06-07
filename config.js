cf38f24fda486d75a4aed4106cc6f7cbfe111100
Nampeth Reungpatikorn <nampeth_reu@freewillsolutions.com>
2026-06-07 17:41:20 +0700

    chore: set production API_URL for GitHub Pages

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.STOCK_APP_CONFIG = {
   // ใส่ URL ที่ได้จาก Google Apps Script Deploy > Web app
-  API_URL: ""
+  API_URL: "https://script.google.com/macros/s/AKfycbwgIpixnzGJu-vFHXoOmIeN3LDkyi6-4e4Qixu1A0hFWTFs5A-AegorX2ZZYhVqgfUHlQ/exec"
 };
